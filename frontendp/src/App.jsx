52ee94aa44a2fdee4534d9737dc0b524b8577490
Anurag <tiwanurag44@gmail.com>
2025-07-11 11:11:38 +0530

    Final App file Update

--- frontendp/src/App.jsx
+++ frontendp/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes,Route } from "react-router"
 import HomePage from "./pages/HomePage"
 import FormPrompt from "./pages/FormPrompt"
 import UserPortfolio from "./pages/UserPortfolio"
+import WebsiteGenerator from "./pages/WebsiteGenerator"
 
 function App() {
   
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<HomePage />}></Route>
       <Route path="/form" element={<FormPrompt />}></Route>
       <Route path="/user/portfolio" element={<UserPortfolio />}></Route>
+      <Route path="/user/website" element={<WebsiteGenerator />}></Route>
      </Routes>
 
     </>
